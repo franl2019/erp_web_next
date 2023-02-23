232b0238b7fcaa41e5bb0e1017e42d8a60727287
franl2019 <1606075736@qq.com>
2023-02-23 15:11:31 +0800

    saleOrderMx render

---
+++ src/view/saleOrder/tableConfig/render/saleOrderMxLineCloseRender.vue
@@ -0,0 +1,17 @@
+<template>
+  <a v-if="$props.params.value === 0" class="font-bold text-red-500">
+    关闭
+  </a>
+</template>
+
+<script lang='ts'>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "SaleOrderMxLineCloseRender",
+  props:["params"],
+  setup() {
+
+  },
+});
+</script>
